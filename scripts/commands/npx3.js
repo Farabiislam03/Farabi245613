9364c094b2e34c85419be83f1ccac686f0f3afaa
joyahmed88 <168209541+joyahmed88@users.noreply.github.com>
2024-04-26 23:15:04 +0600

    Update npx3.js

--- scripts/commands/npx3.js
+++ scripts/commands/npx3.js
@@ -17,7 +17,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
   const axios = require('axios')
-  const NAYAN = ['https://i.imgur.com/LLucP15.mp4','https://i.imgur.com/DEBRSER.mp4']
+  const NAYAN = ['https://i.imgur.com/UREhmOv.mp4','https://i.imgur.com/DEBRSER.mp4']
     var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
 const media = (
     await axios.get(
@@ -26,7 +26,7 @@ const media = (
     )
   ).data;
 
-	if (body.indexOf("🥰")==0 || body.indexOf("🤩")==0 || body.indexOf("😍")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
+	if (body.indexOf("🥰")==0 || body.indexOf("🤩")==0 || body.indexOf("😍")==0 || body.indexOf("😐")==0 || body.indexOf("😅")==0 || body.indexOf("🥹")==0 || body.indexOf("😊")==0 || body.indexOf("🙃")==0 || body.indexOf("🙂")==0 || body.indexOf("😇")==0) {
 		var msg = {
 				body: "🖤🥀",
 				attachment: media
