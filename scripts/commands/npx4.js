b25771e8f885bc5e48ce8d77f33d3420a99aadcb
joyahmed88 <168209541+joyahmed88@users.noreply.github.com>
2024-04-28 16:27:08 +0600

    Create npx4.js

---
+++ scripts/commands/npx4.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "npx4",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: async function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+  const axios = require('axios')
+const media = (
+    await axios.get(
+      'https://drive.google.com/uc?id=1N1-kBpAKdwVY_b48uhBT7Gy7fO98Q2kz',
+      { responseType: 'stream' }
+    )
+  ).data;
+
+	if (body.indexOf("💋")==0 || body.indexOf("😘")==0 || body.indexOf("😽")==0 || body.indexOf("👄")==0 || body.indexOf("💟")==0 || body.indexOf("I love u")==0 || body.indexOf("I love you")==0 || body.indexOf("valobashi")==0 || body.indexOf("Valobashi")==0 || body.indexOf("🖤")==0) {
+		var msg = {
+				body: "🖤",
+				attachment: media
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+  }
+} 
